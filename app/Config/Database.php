c95825916a861a7b65198c00ed38dc2423a5037d
wikel <wikelafrikesmi25@gmail.com>
2026-07-09 23:30:45 +0700

    revisi database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -25,30 +25,30 @@ class Database extends Config
      * @var array<string, mixed>
      */
     public array $default = [
-    'DSN'          => '',
-    'hostname'     => env('database.default.hostname', ''),
-    'username'     => env('database.default.username', ''),
-    'password'     => env('database.default.password', ''),
-    'database'     => env('database.default.database', ''),
-    'DBDriver'     => env('database.default.DBDriver', 'MySQLi'),
-    'DBPrefix'     => '',
-    'pConnect'     => false,
-    'DBDebug'      => true,
-    'charset'      => 'utf8mb4',
-    'DBCollat'     => 'utf8mb4_general_ci',
-    'swapPre'      => '',
-    'encrypt'      => false,
-    'compress'     => false,
-    'strictOn'     => false,
-    'failover'     => [],
-    'port'         => (int) env('database.default.port', 3306),
-    'numberNative' => false,
-    'dateFormat'   => [
-        'date'     => 'Y-m-d',
-        'datetime' => 'Y-m-d H:i:s',
-        'time'     => 'H:i:s',
-    ],
-];
+        'DSN'          => '',
+        'hostname'     => '',
+        'username'     => '',
+        'password'     => '',
+        'database'     => '',
+        'DBDriver'     => 'MySQLi',
+        'DBPrefix'     => '',
+        'pConnect'     => false,
+        'DBDebug'      => true,
+        'charset'      => 'utf8mb4',
+        'DBCollat'     => 'utf8mb4_general_ci',
+        'swapPre'      => '',
+        'encrypt'      => false,
+        'compress'     => false,
+        'strictOn'     => false,
+        'failover'     => [],
+        'port'         => 3306,
+        'numberNative' => false,
+        'dateFormat'   => [
+            'date'     => 'Y-m-d',
+            'datetime' => 'Y-m-d H:i:s',
+            'time'     => 'H:i:s',
+        ],
+    ];
 
     //    /**
     //     * Sample database connection for SQLite3.
