b6c8a4843fe2ee342139619ccd9984c3c7819c20
wikel <wikelafrikesmi25@gmail.com>
2026-07-10 09:52:20 +0700

    revisi

--- app/Controllers/Test.php
+++ app/Controllers/Test.php
@@ -8,12 +8,14 @@ class Test extends BaseController
     {
         echo "<pre>";
 
-        var_dump(env('database.default.hostname'));
-        var_dump(env('database.default.database'));
-        var_dump(env('database.default.username'));
-        var_dump(env('database.default.password'));
-        var_dump(env('database.default.DBDriver'));
-        var_dump(env('database.default.port'));
+        echo "ENV:\n";
+        var_dump($_ENV);
+
+        echo "\nSERVER:\n";
+        var_dump($_SERVER['database.default.hostname'] ?? null);
+
+        echo "\nGETENV:\n";
+        var_dump(getenv('database.default.hostname'));
 
         echo "</pre>";
     }
